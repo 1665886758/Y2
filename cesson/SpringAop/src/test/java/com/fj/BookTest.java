1432dbaefe3847671d38f9f800fe388bbf41b599
1665886758 <1665886758@qq.com>
2018-07-21 16:47:48 +0800

    注解实现-5种增强处理

--- cesson/SpringAop/src/test/java/com/fj/BookTest.java
+++ cesson/SpringAop/src/test/java/com/fj/BookTest.java
@@ -12,9 +12,13 @@ public class BookTest {
     public void Test(){
         ApplicationContext ctx=new ClassPathXmlApplicationContext("app01.xml");
         BookService bookService=(BookService) ctx.getBean("bookService");
+          //1.前置增强 2.后置增强
 //        bookService.bookInfo();
+          //3.异常抛出增强
 //        bookService.throwDemo();
+          //4.最终增强
        // bookService.returnings("张浩");
+           //5.环绕增强
         bookService.login("admin","123");
     }
 }
